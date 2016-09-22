9f06b029490c89763831a50e1ab39354f0b19f00
PEN <petypen@yandex.ru>
2016-09-22 09:14:45 +0300

    добавлена функция fileExsists

---
+++ fileExsists.js
@@ -0,0 +1,15 @@
+﻿// проверить существование файла на диске
+// параметры:
+// cPath - строка. Полный путь к проверяемому файлу.
+// возвращается булево (true - файл существует; false - файл не существует)
+function FileExists(cPath) {
+  var bFileExist;
+  var oFileSystem = new ActiveXObject("Scripting.FileSystemObject");
+
+  if (oFileSystem.FileExists(cPath)) {
+    bFileExist = true
+  } else {
+    bFileExist = false
+  }
+  return bFileExist;
+}
